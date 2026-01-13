d3fc55a52a222d57abbb613f24f65293bdd29886
zufarf <mzufarfainan@gmail.com>
2026-01-13 22:20:50 +0700

    Update stock label in dashboard views to reflect quantity in pieces instead of kilograms

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -25,7 +25,7 @@
                                             </div>
                                         </div>
                                         <div class="col-md-8 col-lg-12 col-xl-12 col-xxl-7">
-                                            <h6 class="text-muted font-semibold">Total Stok (Kg)</h6>
+                                            <h6 class="text-muted font-semibold">Total Stok (Pcs)</h6>
                                             <h6 class="font-extrabold mb-0">{{ $total_pcs ?? 0 }} Pcs</h6>
                                         </div>
                                     </div>
